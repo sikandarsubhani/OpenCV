248b83bca0f3fc2197771d13e3678038e3ff954e
7Sikandar <53407273+sikandarsubhani@users.noreply.github.com>
2023-09-17 23:24:31 +0500

    finds and draws contours on the edges, displaying the result.resizes an image, converts it to grayscale, applies Gaussian blur, and detects edges using the Canny edge detection

--- contours.py
+++ contours.py
@@ -1,17 +1,29 @@
 import cv2 as cv
+import numpy as np
 
+img = cv.resize(cv.imread('C:/Users/navee/Desktop/sikandar/Coding/Python/OpenCV/trace.jpg'), (500,600), interpolation=cv.INTER_CUBIC)
+cv.imshow('Resized', img)
 
-img = cv.resize(cv.imread('C:/Users/navee/Desktop/sikandar/Coding/Python/OpenCV/trace.jpg'), (500,500), interpolation=cv.INTER_CUBIC)
-cv.imshow('Trace', img)
+blank = np.zeros(img.shape,'uint8')
 #gray
 gray_image=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 cv.imshow("Gray", gray_image)
 
-# Canny Edges
-canny=cv.Canny(img,125,175) 
-cv.imshow("Edges", canny)
+# Blur
+blur = cv.GaussianBlur(gray_image,(5,5),cv.BORDER_DEFAULT) # (kernel size , sigmaX,sigmaY)
+cv.imshow("Blurred Image", blur)
 
-# Contours
+# canny Edges
+canny = cv.Canny(blur, 125, 175)
+cv.imshow('Canny Edges', canny)
+
+# ret, thresh = cv.threshold(gray, 125, 255, cv.THRESH_BINARY)
+# cv.imshow('Thresh', thresh)
+
 contours, hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
 print(f'{len(contours)} contour(s) found!')
+
+cv.drawContours(blank, contours, -1, (0,0,255), 1)
+cv.imshow('Contours Drawn', blank)
+
 cv.waitKey(0)
